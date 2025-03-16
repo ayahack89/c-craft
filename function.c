a0d5eaaceee72124521abbe92a9c9aa63d7d48bb
Ayanavbha Chatterjee <117476380+aYgCOO@users.noreply.github.com>
2025-03-16 17:51:26 +0530

    Update

--- function.c
+++ function.c
@@ -1,13 +1,52 @@
 #include<stdio.h>
+#include<string.h>
+
+void Demo(){
+char str1[10];
+printf("Enter a string without space: ");
+scanf("%s", &str1);
+printf("\n%s\n", str1);
+
+}
 
 int Calculator(int number1, int number2){
-int Addition = number1 + number2;
-return Addition;
+char choice[10];
+printf("<+:addition><-:subtraction><*:multiplication></:division> :: Enter your operator: ");
+scanf("%s", choice);
+
+if(strcmp(choice, "+")==0){
+    int add;
+    add = number1 + number2;
+    return add;
+}
+if(strcmp(choice, "-")==0){
+    int sub;
+    sub = number1 - number2;
+    return sub;
+}
+if(strcmp(choice, "*")==0){
+    int multi;
+    multi = number1 * number2;
+    return multi;
+}
+if(strcmp(choice, "/")==0){
+    int div;
+    div = number1 / number2;
+    return div;
+}
 }
 
 int main(){
-int result;
-result = Calculator(3,2);
+
+Demo();
+int number1, number2, result;
+printf("Enter a number: ");
+scanf("%d", &number1);
+printf("Enter another number: ");
+scanf("%d", &number2);
+
+result = Calculator(number1, number2);
 printf("%d", result);
+
 return 0;
 }
